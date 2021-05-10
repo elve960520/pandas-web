533fc8b193d5cd79df788e7ebbea1c909fea32ee
mengfanyu03 <mengfanyu03@58.com>
2021-05-10 23:28:30 +0800

    add(elementui):添加elemeentui基础代码

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Home from './views/Home.vue'
 import router from './router'
 import store from './store'
+//use elementui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(Home)
 }).$mount('#app')
